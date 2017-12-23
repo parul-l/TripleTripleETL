b7a7b47ea6a44dd69ad80f3112aafb8efd2224af
Parul <parul.laul@gmail.com>
2017-12-23 13:56:36 -0500

    Added more tests

--- tests/test_load_postgres_postgrestetl.py
+++ tests/test_load_postgres_postgrestetl.py
@@ -14,7 +14,7 @@ from triple_triple_etl.load.postgres.postgres_etl import (
 from tests.helper_methods import create_mock_context_manager
 
 
-class TestPostgresELT(unittest.TestCase):
+class TestPostgresETL(unittest.TestCase):
     """Tests for postgres_etl.py"""
 
     def test_get_connection(self):
@@ -65,6 +65,31 @@ class TestPostgresELT(unittest.TestCase):
 
         cursor_mock.copy_from.assert_called_once_with(mock_file, 'some', sep=',', null='')
         mock_file.readline.assert_called_once()
+
+    def test_PostgresETL_extract_from_s3(self):
+        tempfile_mock = mock.Mock()
+        bucket_mock = mock.Mock(return_value='nba-player-positions')
+        etl = PostgresETL(filename=tempfile_mock)
+
+        s3download_mock = mock.Mock(return_value='some.txt')
+        extract2dir_mock = mock.Mock()
+
+        tempfile_mock.mkdtemp.return_value = '/tmp/random_letters'
+
+        patches = {
+            'extract2dir': extract2dir_mock,
+            's3download': s3download_mock,
+            'tempfile': tempfile_mock
+        }
+        path = 'triple_triple_etl.load.postgres.postgres_etl'
+        with mock.patch.multiple(path, **patches):
+            directory = etl.extract_from_s3()
+        s3download_mock.assert_called_once_with(
+            bucket_name=bucket_mock.return_value,
+            filename=tempfile_mock
+        )
+        assert etl.filename == tempfile_mock
+        assert directory == '/tmp/random_letters'
 
 
 if __name__ == '__main__':
