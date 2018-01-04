31dea7fe523d081a40555fa9205517aff8826d7c
Parul <parul.laul@gmail.com>
2018-01-03 19:27:32 -0500

    Modified imports and __all__ list for the modified functions

--- triple_triple_etl/core/__init__.py
+++ triple_triple_etl/core/__init__.py
@@ -1,10 +1,12 @@
+from .nbastats_get_data import get_data
+from .nbastats_json2csv import get_all_nbastats_tables
 from .s3 import s3download, extract2dir
-from .json2csv import get_all_tables_dict, save_all_tables
+from .s3_json2csv import get_all_tables_dict
 
 
 __all__ = [
+    'get_data',
     's3download',
     'extract2dir',
-    'get_all_tables_dict',
-    'save_all_tables'
+    'get_all_tables_dict'
 ]
