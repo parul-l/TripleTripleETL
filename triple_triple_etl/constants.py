234d2737c2d01010f0ba7c0af032b7b8b7af28a8
Parul <parul.laul@gmail.com>
2018-01-05 11:19:49 -0500

    Added base_url_play and base_url_box_score to constants.py file

--- triple_triple_etl/constants.py
+++ triple_triple_etl/constants.py
@@ -12,5 +12,10 @@ config.read(os.path.join(MODULE_HOME, 'params.cfg'))
 DATATABLES_DIR = os.path.abspath(config['path']['datatables_dir'])
 DATASETS_DIR = os.path.abspath(config['path']['datasets_dir'])
 
+
+BASE_URL_PLAY = 'http://stats.nba.com/stats/playbyplayv2'
+BASE_URL_BOX_SCORE_TRADITIONAL = 'http://stats.nba.com/stats/boxscoretraditionalv2'
+
+
 MAX_STORAGE_MB = 1000
 SCHEMA_DIR = os.path.join(MODULE_HOME, 'load', 'schemata')
