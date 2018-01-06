216314d2427d1e1edfeaf7441a23f74049f56acc
Parul <parul.laul@gmail.com>
2018-01-06 17:25:31 -0500

    Changed import syntax and file to add in script

--- scripts/run_s3_and_playbyplay_postgres_etl.py
+++ scripts/run_s3_and_playbyplay_postgres_etl.py
@@ -1,7 +1,11 @@
 from triple_triple_etl.core.s3 import get_game_files
 from triple_triple_etl.load.postgres.s3_postgres_etl import S3PostgresETL
-from triple_triple_etl.load.postgres.nbastats_postgres_etl import NBAStatsPostgresETL
-from triple_triple_etl.load.postgres.s3_and_playbyplay_postgres_etl import S3andPlaybyPlayPostgresETL
+from triple_triple_etl.load.postgres.nbastats_postgres_etl import(
+    NBAStatsPostgresETL
+) 
+from triple_triple_etl.load.postgres.s3_and_playbyplay_postgres_etl import (
+    S3andPlaybyPlayPostgresETL
+)
 from triple_triple_etl.constants import BASE_URL_PLAY
 
 if __name__ == '__main__':
@@ -25,7 +29,7 @@ if __name__ == '__main__':
 
     all_files = get_game_files(bucket_name)
 
-    for filename in all_files[5:6]:
+    for filename in all_files[6:7]:
         s3_etl = S3PostgresETL(filename=filename)
         playbyplay_etl = NBAStatsPostgresETL(**pbp_input)
         etl = S3andPlaybyPlayPostgresETL(
