9eabcdf36f201cdff65bc1087c46286834d483c9
Parul <parul.laul@gmail.com>
2018-01-07 13:23:06 -0500

    Added get_primary_keys function

--- triple_triple_etl/load/postgres/postgres_helper.py
+++ triple_triple_etl/load/postgres/postgres_helper.py
@@ -1,8 +1,21 @@
 import os
+import yaml
 
-from triple_triple_etl.constants import DATATABLES_DIR
+from triple_triple_etl.constants import DATATABLES_DIR, SCHEMA_DIR
 from triple_triple_etl.load.postgres.postgres_connection import get_cursor
 
+
+def get_primary_keys(tablename, schema_file='position_data_tables.yaml'):
+    filename = os.path.join(SCHEMA_DIR, schema_file)
+
+    with open(filename, 'r') as schema_file:
+        schema = yaml.load(schema_file)
+
+    def primary_key(c):
+        return 'primary key' in c.get('constraints', {})
+
+    columns = schema['tables'][tablename]['columns']
+    return map(lambda x: x['name'], filter(primary_key, columns))
 
 def csv2postgres(filepath):
     with get_cursor() as cursor:
