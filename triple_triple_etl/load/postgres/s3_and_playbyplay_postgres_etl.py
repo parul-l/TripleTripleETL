3164eb167127a0be4cc68128047337d92c6577bd
Parul <parul.laul@gmail.com>
2018-01-08 12:46:18 -0500

    Removed unused imports, and corrected game_id to GameID

--- triple_triple_etl/load/postgres/s3_and_playbyplay_postgres_etl.py
+++ triple_triple_etl/load/postgres/s3_and_playbyplay_postgres_etl.py
@@ -1,8 +1,3 @@
-from triple_triple_etl.load.postgres.s3_postgres_etl import S3PostgresETL
-from triple_triple_etl.load.postgres.nbastats_postgres_etl import NBAStatsPostgresETL
-from triple_triple_etl.constants import BASE_URL_PLAY
-
-
 class S3andPlaybyPlayPostgresETL(object):
     def __init__(
         self,
@@ -17,7 +12,7 @@ class S3andPlaybyPlayPostgresETL(object):
        
         # update game_id and season from s3 data
         # for inputs to playbyplay_etl
-        self.playbyplay_etl.params['game_id'] = self.s3_etl.game_id
+        self.playbyplay_etl.params['GameID'] = self.s3_etl.game_id
         self.playbyplay_etl.params['season'] = self.s3_etl.season
         self.playbyplay_etl.run()
 
