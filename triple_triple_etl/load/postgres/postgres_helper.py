96d2c7e325823e2ac107e04068883119447c3621
Parul <parul.laul@gmail.com>
2018-01-08 12:45:23 -0500

    Created separate csv2postgres functions depending on whether table had primary keys or not

--- triple_triple_etl/load/postgres/postgres_helper.py
+++ triple_triple_etl/load/postgres/postgres_helper.py
@@ -17,13 +17,59 @@ def get_primary_keys(tablename, schema_file='position_data_tables.yaml'):
     columns = schema['tables'][tablename]['columns']
     return map(lambda x: x['name'], filter(primary_key, columns))
 
-def csv2postgres(filepath):
+
+def csv2postgres_pkeys(tablename, filepath, schema_file):
+    pkeys_params = {'tablename': tablename, 'schema_file': schema_file}
+    pkeys = ','.join(get_primary_keys(**pkeys_params))
+    
+    query = """
+        BEGIN TRANSACTION;
+        
+        CREATE TABLE {tablename}_staging(LIKE {tablename});
+        
+        COPY {tablename}_staging
+        FROM STDIN
+        DELIMITER ',' CSV HEADER;
+        
+        INSERT INTO {tablename}
+          SELECT DISTINCT ON ({primary_key_fields}) *
+          FROM {tablename}_staging
+        ON CONFLICT ({primary_key_fields}) DO NOTHING;
+        
+        DROP TABLE {tablename}_staging;
+        
+        END TRANSACTION;
+    """.format(tablename=tablename, primary_key_fields=pkeys)
+
     with get_cursor() as cursor:
         with open(filepath) as f:
-            f.readline()
-            tablename = os.path.basename(filepath).replace('.csv', '')
-            cursor.copy_from(f, tablename, sep=',', null='')
+            cursor.copy_expert(query, f)
 
+
+def csv2postgres_no_pkeys(filepath):
+    tablename = os.path.basename(filepath).replace('.csv', '')
+    query = """
+        BEGIN TRANSACTION;
+        
+        CREATE TABLE {tablename}_staging(LIKE {tablename});
+        
+        INSERT INTO {tablename}_staging
+          SELECT DISTINCT *
+          FROM {tablename};
+        
+        DROP TABLE {tablename};
+        
+        ALTER TABLE {tablename}_staging
+          RENAME TO {tablename};
+        
+        END TRANSACTION;
+    """.format(tablename=tablename)
+
+    with get_cursor() as cursor:
+        with open(filepath) as f:
+            f.readline()        
+            cursor.copy_from(f, tablename, sep=',', null='')
+            cursor.execute(query)
 
 def save_all_tables(tables_dict, storage_dir=DATATABLES_DIR):
     for name, df in tables_dict.items():
