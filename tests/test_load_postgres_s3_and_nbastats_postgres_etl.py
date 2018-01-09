bdbe5f473555e52b12a8e0c29e38b82791941c32
Parul <parul.laul@gmail.com>
2018-01-09 17:20:50 -0500

    Added tests to test boxscore_traditional and boxscore_player_tracking to etl

--- tests/test_load_postgres_s3_and_nbastats_postgres_etl.py
+++ tests/test_load_postgres_s3_and_nbastats_postgres_etl.py
@@ -1,14 +1,13 @@
 import unittest
-
 import mock
 
-from triple_triple_etl.load.postgres.s3_and_playbyplay_postgres_etl import
+from triple_triple_etl.load.postgres.s3_and_nbastats_postgres_etl import
     (
         S3andNBAStatsPostgresETL
     )
 
 class TestS3andNBAStatsPostgresETL(unittest.TestCase):
-    """ Test for s3_and_playbyplay_postgres_etl.py"""
+    """ Test for s3_and_nbastats_postgres_etl.py"""
 
     def test_run(self):
         s3_etl_mock = mock.Mock()
@@ -21,16 +20,35 @@ class TestS3andNBAStatsPostgresETL(unittest.TestCase):
         playbyplay_etl_mock.params = params
         playbyplay_etl_mock.run = mock.Mock()
 
+        boxscore_traditional_etl_mock = mock.Mock()
+        boxscore_traditional_etl_mock.params = params
+        boxscore_traditional_etl_mock.run = mock.Mock()
+
+        boxscore_player_tracking_etl_mock = mock.Mock()
+        boxscore_player_tracking_etl_mock.params = params
+        boxscore_player_tracking_etl_mock.run = mock.Mock()
+
+
         etl = S3andNBAStatsPostgresETL(
             s3_etl=s3_etl_mock,
-            playbyplay_etl=playbyplay_etl_mock
+            playbyplay_etl=playbyplay_etl_mock,
+            boxscore_traditional_etl=boxscore_traditional_etl_mock,
+            boxscore_player_tracking_etl=boxscore_player_tracking_etl_mock
         )
         etl.run()
         
         s3_etl_mock.run.assert_called_once_with()
-        playbyplay_etl_mock.run.assert_called_once_with()
-        assert playbyplay_etl_mock.params['game_id'] == '1234'
-        assert playbyplay_etl_mock.params['season'] == '2480-81'
+        
+        nbastats_etls = [
+            playbyplay_etl_mock,
+            boxscore_traditional_etl_mock,
+            boxscore_player_tracking_etl_mock
+        ]
+        for nba_etl in nbastats_etls:
+
+            nba_etl.run.assert_called_once_with()
+            assert nba_etl.params['game_id'] == '1234'
+            assert nba_etl.params['season'] == '2480-81'
 
 
 if __name__ == '__main__':
