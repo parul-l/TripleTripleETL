217460a2b6476a37b2906d5b8f71d70a090157a6
Parul <parul.laul@gmail.com>
2019-03-04 14:55:42 -0500

    Changed HEADERS to reflect new info on stats.nba.com

--- triple_triple_etl/core/nbastats_get_data.py
+++ triple_triple_etl/core/nbastats_get_data.py
@@ -1,12 +1,11 @@
 import requests
 
 HEADERS = {
-    'user-agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) '
+    'user-agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) '
                    'AppleWebKit/537.36 (KHTML, like Gecko) '
-                   'Chrome/45.0.2454.101 Safari/537.36'),
+                   'Chrome/71.0.3578.98 Safari/537.36'),
     'referer': 'http://stats.nba.com/player/'
 }
-
 
 def get_data(base_url, params, headers=HEADERS):
     response = requests.get(url=base_url, params=params, headers=headers)
