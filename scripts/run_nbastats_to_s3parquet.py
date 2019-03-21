47f4b649f243fb22c0789412fdbc8ccfe825ac0a
Parul <parul.laul@gmail.com>
2019-03-21 18:13:38 -0400

    Reworked nba game_log etl

--- scripts/run_nbastats_to_s3parquet.py
+++ scripts/run_nbastats_to_s3parquet.py
@@ -4,16 +4,18 @@ from triple_triple_etl.load.storage.nbastats_to_s3parquet import (
 )
 
 if __name__ == '__main__':
-    # first_gamedate = get_first_date_season(
-    #     season='2015-16',
-    #     datefrom='10/01/2015',
-    #     dateto='10/31/2016',        
-    # )
-    datefrom = first_gamedate
-    dateto = first_gamedate
+    first_gamedate = get_first_date_season(
+        season='2015-16',
+        datefrom='10/01/2015',
+        dateto='10/31/2015',        
+    )
+    # datefrom = first_gamedate
+    # dateto = first_gamedate
     
     etl = NBAStatsGameLogsS3ETL(
-        datefrom=test_date,
-        dateto=test_date,
+        datefrom=first_gamedate,
+        dateto=first_gamedate,
         season_year='2015-16',
     )
+
+    etl.run()
--- triple_triple_etl/load/storage/nbastats_to_s3parquet.py
+++ triple_triple_etl/load/storage/nbastats_to_s3parquet.py
@@ -2,6 +2,8 @@ import boto3
 import datetime
 import os
 import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
 import re
 import tempfile
 import shutil
@@ -47,44 +49,22 @@ def get_first_date_season(
     return '{}/{}/{}'.format(date[1], date[2], date[0])
 
 
-def add_metadata(
-        df: pd.DataFrame,
-        data_dict: dict
-):
-    for col, val in data_dict.items():
-        df[col] = val
-    return df
-
-
-def tmp_save_gamelog(df: pd.DataFrame):
-    tmp_dir = tempfile.mkdtemp()
-    filename = 'gamelog{}.parquet.snappy'.format(df.GAME_ID.loc[0])
-    filepath = os.path.join(tmp_dir, filename)
-    df.to_parquet(fname=filepath, compression='snappy')
-
-    return filepath
-
-
-def get_game_info(df: pd.DataFrame):
-    gamedate_ = df.GAME_DATE.loc[0].split('-')
-
-    return {
-        'gameid': df.GAME_ID.loc[0],
-        'matchup': re.sub(r'\W+', 'at', df.MATCHUP.loc[0]),
-        'gamedate': '{}{}{}'.format(gamedate_[1], gamedate_[2], gamedate_[0])
-    }
-
-
 def append_upload_metadata(
-        df: pd.DataFrame,
-        data: list,
-        columns: list,
+        df_data: pd.DataFrame,
+        df_upload: pd.DataFrame,
+        lastuploadDTS: str,
+        base_url: str,
+        params: str,
         uploadFLG: bool
 ):
-    data.extend([uploadFLG])
-    df_new = pd.DataFrame([data], columns=columns)
-    
-    return df.append(df_new, ignore_index=True)
+    update_cols = ['season', 'game_date', 'game_id']
+    df_add = df_data[update_cols]
+    df_add.loc[:, 'gamelog_uploadedFLG'] = uploadFLG
+    df_add.loc[:, 'lastuploadDTS'] = lastuploadDTS
+    df_add.loc[:, 'base_url'] = base_url
+    df_add.loc[:, 'params'] = str(params)
+
+    return pd.concat([df_upload, df_add], axis=0, ignore_index=True)
 
 
 class NBAStatsGameLogsS3ETL(object):
@@ -100,7 +80,8 @@ class NBAStatsGameLogsS3ETL(object):
         self.dateto = dateto
         self.season_year = season_year
         self.destination_bucket = destination_bucket
-        self.tmp_dirs = []
+        self.tmp_dir = None
+        self.loaddate = datetime.datetime.utcnow().strftime('%F %TZ')
 
         # api info
         self.base_url = BASE_URL_GAMELOG
@@ -123,8 +104,9 @@ class NBAStatsGameLogsS3ETL(object):
         logger.info('Getting loaded files metadata')
         # meta data columns
         columns = [
-            'base_url', 'params', 'season', 'gamedate',
-            'gameid', 'lastuploadDTS','gamelog_uploadedFLG',
+            'season', 'game_date', 'game_id',
+            'gamelog_uploadedFLG', 'lastuploadDTS',
+            'base_url', 'params'
         ]
         self.df_uploaded = get_uploaded_metadata(
             self.uploaded_filepath,
@@ -134,76 +116,89 @@ class NBAStatsGameLogsS3ETL(object):
     def extract_by_date_from_nbastats(self):
         logger.info('Get gamelogs from {} to {}'.format(self.datefrom, self.dateto))
         return get_data(base_url=self.base_url, params=self.params)
-        
-    def load(self, data):
-        loaddate = datetime.datetime.utcnow().strftime('%F %TZ')
-        metadata_info = {
-            'BASE_URL': self.base_url,
-            'PARAMS': str(self.params),
-            'UPDATE_DTS': loaddate
-        }
-        for games in data['resultSets']:
-        # data['resultSets'] is a list of list. 
-        # Usually data[resultSets] has 1 element
-            for game in games['rowSet']:
-                # create df
-                df = add_metadata(
-                    df=pd.DataFrame(data=[game], columns=games['headers']),
-                    data_dict=metadata_info
-                )
-                # save to tmp dir and collect tmpdir path
-                filepath = tmp_save_gamelog(df)
-                self.tmp_dirs.append(os.path.dirname(filepath))
 
-                # get info for metadata
-                gameinfo = get_game_info(df)
-                updatemetadata = [
-                    self.base_url,
-                    str(self.params),
-                    self.season_year,
-                    gameinfo['gamedate'],
-                    gameinfo['gameid'],
-                    loaddate
-                ]
-                try:
-                    # load to s3
-                    logger.info('Uploading gamelog {} to s3'.format(gameinfo['gameid']))
-                    s3.upload_file(
-                        Filename=filepath,
-                        Bucket=self.destination_bucket,
-                        Key='gamelog/season={}/gameid={}/{}{}.parquet.snappy'.format(
-                            self.season_year,
-                            gameinfo['gameid'],
-                            gameinfo['gamedate'],
-                            gameinfo['matchup']    
-                        )
-                    )
-                    # update metadata
-                    self.df_uploaded = append_upload_metadata(
-                        df=self.df_uploaded,
-                        data=updatemetadata,
-                        columns=self.df_uploaded.columns,
-                        uploadFLG=1
-                    )
 
-                except (boto3.exceptions.botocore.client.ClientError, FileNotFoundError) as err:
-                    logger.error(err)
-                    # update metadata
-                    self.df_uploaded = append_upload_metadata(
-                        df=self.df_uploaded,
-                        data=updatemetadata,
-                        columns=self.df_uploaded.columns,
-                        uploadFLG=0
-                    )
-                    continue          
+    def transform(self, data):
+        all_dfs = [
+            pd.DataFrame(
+                data=games['rowSet'],
+                columns=[x.lower() for x in games['headers']]
+            ) 
+            for games in data['resultSets']
+        ]
+        # join all dfs (usually just one), add season
+        df = pd.concat(all_dfs, ignore_index=True)
+        df.loc[:, 'season'] = self.season_year
+
+        self.tmp_dir = tempfile.mkdtemp()
+        pq.write_to_dataset(
+            table=pa.Table.from_pandas(df, preserve_index=False),
+            root_path=self.tmp_dir,
+            partition_cols=['season', 'game_id'],
+            compression='snappy',
+            preserve_index=False
+        )    
             
+        return df        
+
+    def load(self, df):
+
+        try:
+            table_dir = os.path.join(self.tmp_dir, 'season={}'.format(self.season_year))
+            for game in os.listdir(table_dir):
+                gameid = game.split('=')[1]
+                df_game = df.query('game_id == @gameid')
+                matchup = re.findall(r'[\w]+', df_game.matchup.iloc[0])
+                gamedate = ''.join(df_game.game_date.iloc[0].split('-'))
+
+                game_path = os.path.join(table_dir, game)
+                filepath = os.path.join(game_path, os.listdir(game_path)[0])
+           
+                # load to s3
+                logger.info('Uploading gamelog {} to s3'.format(gameid))
+                s3.upload_file(
+                    Filename=filepath,
+                    Bucket=self.destination_bucket,
+                    Key='gamelog/season={}/gameid={}/{}{}{}.parquet.snappy'.format(
+                        self.season_year,
+                        gameid,
+                        gamedate,
+                        matchup[0],
+                        matchup[-1]
+                    )
+                )
+                # update metadata
+                self.df_uploaded = append_upload_metadata(
+                    df_data=df_game,
+                    df_upload=self.df_uploaded,
+                    lastuploadDTS=self.loaddate,
+                    base_url=self.base_url,
+                    params=self.params,
+                    uploadFLG=1
+                )
+
+        except (boto3.exceptions.botocore.client.ClientError, FileNotFoundError) as err:
+            logger.error(err)
+            # update metadata
+            self.df_uploaded = append_upload_metadata(
+                df_data=df,
+                df_upload=self.df_uploaded,
+                lastuploadDTS=self.lastuploadDTS,
+                base_url=self.base_url,
+                params=self.params,
+                uploadFLG=0
+            )
+
     def cleanup(self):
         # remove tmp directories
-        for tmp in self.tmp_dirs:
-            shutil.rmtree(tmp)
+        shutil.rmtree(self.tmp_dir)
         
         # save df_uploaded
-        self.df_uploaded.to_parquet(fname=self.uploaded_filepath, compression='snappy')
+        self.df_uploaded.to_parquet(
+            fname=self.uploaded_filepath,
+            compression='snappy',
+            index=False
+        )
 
     def run(self):
         # etl start time
@@ -211,7 +206,8 @@ class NBAStatsGameLogsS3ETL(object):
 
         self.metadata()
         data = self.extract_by_date_from_nbastats()
-        self.load(data)
+        df = self.transform(data)
+        self.load(df)
         self.cleanup()
 
         # etl end time
