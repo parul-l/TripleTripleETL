e882db8662fe036452528a517042b28391a67611
Parul <parul.laul@gmail.com>
2019-04-09 09:53:27 -0400

    Corrected imports

--- triple_triple_etl/core/s3.py
+++ triple_triple_etl/core/s3.py
@@ -1,4 +1,6 @@
+import datetime
 import os
+import pandas as pd
 import re
 import tempfile
 
@@ -13,7 +15,7 @@ s3 = boto3.resource('s3')
 REGEX = re.compile("(.+/rawdata/)(\d.+\d.)([a-zA-Z])")
 
 
-def get_game_files(bucket_name):
+def get_game_files(bucket_name: str, save_name: str):
     bucket = s3.Bucket(bucket_name)
     
     logger.info('Getting list of .7z files')
@@ -31,11 +33,11 @@ def get_game_files(bucket_name):
     )
 
     # save the data
-    df_all_files.to_parquet(os.path.join(META_DIR, 'rawpositiondata'))
+    df_all_files.to_parquet(os.path.join(META_DIR, save_name))
     
 
 
-def rename_game_files(bucket_name):
+def rename_game_files(bucket_name: str):
     # restructuring "folder" structure
     all_files = get_game_files(bucket_name)
     for file in all_files:
@@ -52,7 +54,7 @@ def rename_game_files(bucket_name):
 
 
 
-def s3download(bucket_name, filename):
+def s3download(bucket_name: str, filename: str):
     # gettempdir() returns the name of the directory used for temporary files
     logger.info('Creating tmp directory')
     temp_name = filename.replace('/', '.')
@@ -69,7 +71,7 @@ def s3download(bucket_name, filename):
         raise
 
 
-def extract2dir(filepath, directory=DATASETS_DIR):
+def extract2dir(filepath: str, directory: str = DATASETS_DIR):
     try:
         logger.info('Unzip file as .json')
         patoolib.extract_archive(filepath, outdir=directory)
