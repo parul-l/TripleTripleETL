09a6f825af41fa78138121f82792a6d75d79a2a0
Parul <parul.laul@gmail.com>
2019-06-14 17:58:10 -0400

    script to run closest_to_ball_rank_athena_to_s3parquet

--- scripts/run_closest_to_ball_rank_athena_to_s3parquet.py
+++ scripts/run_closest_to_ball_rank_athena_to_s3parquet.py
@@ -17,34 +17,12 @@ if __name__ == '__main__':
         for x in get_bucket_content(bucket_name, prefix_game)
     ]
 
-    # closest_to_ball_gameids = [
-    #     x['Prefix'].split('gameid=')[1][:-1]
-    #     for x in get_bucket_content(bucket_name, prefix_closest)
-    # ]
-
-    # games_to_load = sorted(list(set(gameposition_gameids) - set(closest_to_ball_gameids)))
-
-    for i in range(0, len(gameposition_gameids), 50):
-        block = gameposition_gameids[i: i + 50]
-
+    for i in range(570, len(gameposition_gameids), 10):
+        block = sorted(gameposition_gameids)[i: i + 10]
         etl = ClosestToBallETL(
             season=season,
             gameid_bounds=[block[0], block[-1]]
         )
         etl.run()
-
-
-    # etl = ClosestToBallETL(
-    #     season=season,
-    #     gameid_bounds=['0021500338', '0021500338']
-    # )
-    # '0021500523'
-
-
-    # idx_list = list(np.arange(3, 300, 1))
-    # run_all_closest_to_ball(
-    #     all_games=gameposition_gameids,
-    #     idx=idx_list
-    # )
 
 
